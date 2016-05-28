610ce9974eef3c39c7bf567d5804774a02e38fc2
“jhondominguez1” <jjjdomi@gmail.com>
2016-05-28 14:42:09 -0500

    Adicionamos desde local un archivo llamado index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\prueba201
+
+28/05/2016  02:41 p. m.    <DIR>          .
+28/05/2016  02:41 p. m.    <DIR>          ..
+28/05/2016  02:41 p. m.                 0 index.php
+               1 archivos              0 bytes
+               2 dirs  58.386.444.288 bytes libres
